92f54213b4d8d0a951bf10e07478ed0da9d0528d
BrandyBecker <BrandyBeckerFSW@Gmail.com>
2019-11-13 13:29:47 -0500

    Missing ! from user.username

--- users/users-helpers.js
+++ users/users-helpers.js
@@ -6,12 +6,12 @@ function validateUser(user){
 
     let errors = [];
 
-    if(user.username || user.username.length < 2 ){
+    if(!user.username || user.username.length < 2 ){
     errors.push('please include a username with atleast 2 characters')
     }
     
 
-    if(user.username || user.username.length < 4){
+    if(!user.username || user.username.length < 4){
     errors.push('please include a password with atleast 4 characters')
     }
     
